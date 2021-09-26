e927853a340ec09380e9db13780580623ff82414
unknown <divyansh.hb@gmail.com>
2021-09-26 10:09:22 +0530

    change in dev branch

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,7 +1,7 @@
 public class HelloWorld{
 	public static void main(String[] args){
-		System.out.println("checking for commit");
-		System.out.println("checking for commit");
+		System.out.println("Hello world");
+		System.out.println("commit in dev branch");
 		
 }
 	
